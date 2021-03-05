2678a78373e6580a01e29dcd9223f3594fd2a38e
anapa96 <ana.p.a@yandex.ru>
2021-03-06 00:38:50 +0300

    Урок4. Верстка hero + шрифты. Вывод постов. Импорт

---
+++ wp-content/themes/universal-theme/front-page.php
@@ -0,0 +1,91 @@
+<?php get_header();?>
+<main class="front-page-header">
+  <div class="container">
+    <div class="hero">
+      <div class="left">
+      <?php
+    //объявляем глобальную переменную 
+global $post;
+
+//get_posts отраляем в бд запрос
+$myposts = get_posts([ 
+  //возьми 5 последних постов
+	'numberposts' => 1
+  
+]);
+//проверяем, есть ли посты
+if( $myposts ){
+  //если есть, запускаем цыкл
+	foreach( $myposts as $post ){
+		setup_postdata( $post );
+		?>
+    <!--Выводим записи-->
+      <img src="<?php the_post_thumbnail_url(); ?>" alt="" class="post-thumb">
+      <?php $author_id=get_the_author_meta('ID') ?>
+ 
+     <a href="<?php echo get_author_posts_url($author_id)?>"class="author">
+        <img src="<?php echo get_avatar_url('ID')?>" alt="" class="avatar">
+        <div class="author-bio">
+          <span class="author-name"><?php the_author();?></span>
+          <span class="author-rank">Должность</span>
+        </div>
+      </a>
+      <div class="post-text">
+        <?php the_category()?>
+        <h2 class="post-title"><?php the_title()?></h2>
+        <a href="<?php echo get_the_permalink(); ?>" class="more">Читать далее</a>
+      </div>
+      <?php 
+	}
+} else {
+	?><p>Постов нет</p><?php // Постов не найдено
+}
+
+wp_reset_postdata(); // Сбрасываем $post
+?>
+      </div>
+      <!-- /.left -->
+      <div class="right">
+        <h3 class="recommend">Рекомендуем</h3>
+        <ul class="posts-list">
+        <?php
+    //объявляем глобальную переменную 
+global $post;
+
+//get_posts отраляем в бд запрос
+$myposts = get_posts([ 
+  //возьми 5 последних постов
+	'numberposts' => 5,
+  'offset' => 1,
+  
+]);
+//проверяем, есть ли посты
+if( $myposts ){
+  //если есть, запускаем цыкл
+	foreach( $myposts as $post ){
+		setup_postdata( $post );
+		?>
+          <li class="post">
+            <?php the_category()?>
+            <a class="post-permalink" href="<?php echo get_the_permalink()?>"> <h4 class="post-title"><?php the_title()?></h4></a>
+          </li>
+          <?php 
+	}
+} else {
+	?><p>Постов нет</p><?php // Постов не найдено
+}
+
+wp_reset_postdata(); // Сбрасываем $post
+?>
+        </ul>
+      </div>
+      <!-- /.right -->
+    </div>
+    <!-- /.hero -->
+
+   
+	
+
+  </div>
+  <!-- /.container -->
+</main>
--- wp-content/themes/universal-theme/functions.php
+++ wp-content/themes/universal-theme/functions.php
@@ -8,6 +8,9 @@ if ( ! function_exists( 'universal_theme_setup' ) ) :
     //добавляем тег title
     add_theme_support( 'title-tag' );
 
+    //добавление миниатюр в админку к записям
+    add_theme_support( 'post-thumbnails', array( 'post' ) );
+
     //добавление пользовательского логотипа
     add_theme_support( 'custom-logo', [
       'width'       => 163,
@@ -35,5 +38,6 @@ function enqueue_universal_style() {
 	wp_enqueue_style( 'style', get_stylesheet_uri() );
                   /*название, где лежит, от каких файлов зависит, версия, для каких типов устройств*/
   wp_enqueue_style( 'universal-theme', get_template_directory_uri(  ) . '/assets/css/universal-theme.css', 'style');
+  wp_enqueue_style( 'Roboto-Slab', 'https://fonts.googleapis.com/css2?family=Roboto+Slab:wght@700&display=swap');
 }
 add_action( 'wp_enqueue_scripts', 'enqueue_universal_style' );
