9159952d78c42c9ba465a1fe1f860934c53f2406
anapa96 <ana.p.a@yandex.ru>
2021-03-12 03:04:15 +0300

    Урок 8. Добавила облако тегов в сайдбар и создала виджет соцсетей

--- wp-content/themes/universal-theme/functions.php
+++ wp-content/themes/universal-theme/functions.php
@@ -197,6 +197,165 @@ add_action( 'widgets_init', 'register_downloader_widget' );
 
 
 
+/**
+ * Добавление нового виджета Social_Network_Widget
+ */
+class Social_Network_Widget extends WP_Widget {
+
+	// Регистрация виджета используя основной класс
+	function __construct() {
+		// вызов конструктора выглядит так:
+		// __construct( $id_base, $name, $widget_options = array(), $control_options = array() )
+		parent::__construct(
+			'social_network_widget', // ID виджета, если не указать (оставить ''), то ID будет равен названию класса в нижнем регистре: social_network_widget
+			'Наши соцсети',
+			array( 'description' => 'Ссылки на социальные сети','classname' => 'widget-social-network',)
+		);
+
+		// скрипты/стили виджета, только если он активен
+		if ( is_active_widget( false, false, $this->id_base ) || is_customize_preview() ) {
+			add_action('wp_enqueue_scripts', array( $this, 'add_social_network_widget_scripts' ));
+			add_action('wp_head', array( $this, 'add_social_network_widget_style' ) );
+		}
+	}
+
+	/**
+	 * Вывод виджета во Фронт-энде
+	 *
+	 * @param array $args     аргументы виджета.
+	 * @param array $instance сохраненные данные из настроек
+	 */
+	function widget( $args, $instance ) {
+		$title = $instance['title'];
+		$link_facebook = $instance['link_facebook'];
+		$link_twitter = $instance['link_twitter'];
+		$link_youtube = $instance['link_youtube'];
+		$link_instagram = $instance['link_instagram'];
+		$link_vk = $instance['link_vk'];
+
+
+		echo $args['before_widget'];
+		if ( ! empty( $title ) ) {
+			echo $args['before_title'] . $title . $args['after_title'];
+		}
+		if ( ! empty( $link_facebook ) ) {
+			echo '<a href="' . $link_facebook . '" target="_blank"><img src="' .get_template_directory_uri() . '/assets/images/social-icon/facebook.svg"></a>';
+		}
+		if ( ! empty( $link_twitter ) ) {
+			echo '<a href="' . $link_twitter . '" target="_blank"><img src="' .get_template_directory_uri() . '/assets/images/social-icon/twitter.svg"></a>';
+		}
+		if ( ! empty( $link_youtube ) ) {
+			echo '<a href="' . $link_youtube . '" target="_blank"><img src="' .get_template_directory_uri() . '/assets/images/social-icon/youtube.svg"></a>';
+		}
+		if ( ! empty( $link_instagram ) ) {
+			echo '<a href="' . $link_instagram . '" target="_blank"><img src="' .get_template_directory_uri() . '/assets/images/social-icon/instagram.svg"></a>';
+		}
+		if ( ! empty( $link_vk ) ) {
+			echo '<a href="' . $link_vk. '" target="_blank"><img src="' .get_template_directory_uri() . '/assets/images/social-icon/vk.svg"></a>';
+		}
+		echo $args['after_widget'];
+	}
+	
+
+	/**
+	 * Админ-часть виджета
+	 *
+	 * @param array $instance сохраненные данные из настроек
+	 */
+	function form( $instance ) {
+		$title = @ $instance['title'] ?: 'Наши соцсети';
+		$link_facebook = @ $instance['link_facebook'] ?: 'https://facebook.com/MarilynManson/';
+		$link_twitter = @ $instance['link_twitter'] ?: 'https://twitter.com/marilynmanson';
+		$link_youtube = @ $instance['link_youtube'] ?: 'https://www.youtube.com/channel/UCbirjI1K3MGu0-Y1gTBNR5w';
+		$link_instagram = @ $instance['link_instagram'] ?: 'https://www.instagram.com/marilynmanson/';
+		$link_vk = @ $instance['link_vk'] ?: 'https://vk.com/marilynmanson';
+		
+		?>
+		<p>
+			<label for="<?php echo $this->get_field_id( 'title' ); ?>"><?php _e( 'Заголовок:' ); ?></label> 
+			<input class="widefat" id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" type="text" value="<?php echo esc_attr( $title ); ?>">
+		</p>
+		<p>
+			<label for="<?php echo $this->get_field_id( 'link_facebook' ); ?>"><?php _e( 'Фейсбук:' ); ?></label> 
+			<input class="widefat" id="<?php echo $this->get_field_id( 'link_facebook' ); ?>" name="<?php echo $this->get_field_name( 'link_facebook' ); ?>" type="text" value="<?php echo esc_attr( $link_facebook ); ?>">
+		</p>
+		<p>
+			<label for="<?php echo $this->get_field_id( 'link_twitter' ); ?>"><?php _e( 'Твитер:' ); ?></label> 
+			<input class="widefat" id="<?php echo $this->get_field_id( 'link_twitter' ); ?>" name="<?php echo $this->get_field_name( 'link_twitter' ); ?>" type="text" value="<?php echo esc_attr( $link_twitter ); ?>">
+		</p>
+		<p>
+			<label for="<?php echo $this->get_field_id( 'link_youtube' ); ?>"><?php _e( 'Ютуб:' ); ?></label> 
+			<input class="widefat" id="<?php echo $this->get_field_id( 'link_youtube' ); ?>" name="<?php echo $this->get_field_name( 'link_youtube' ); ?>" type="text" value="<?php echo esc_attr( $link_youtube ); ?>">
+		</p>
+		<p>
+			<label for="<?php echo $this->get_field_id( 'link_instagram' ); ?>"><?php _e( 'Инстаграм:' ); ?></label> 
+			<input class="widefat" id="<?php echo $this->get_field_id( 'link_instagram' ); ?>" name="<?php echo $this->get_field_name( 'link_instagram' ); ?>" type="text" value="<?php echo esc_attr( $link_instagram ); ?>">
+		</p>
+		<p>
+			<label for="<?php echo $this->get_field_id( 'link_vk' ); ?>"><?php _e( 'ВКонтакте:' ); ?></label> 
+			<input class="widefat" id="<?php echo $this->get_field_id( 'link_vk' ); ?>" name="<?php echo $this->get_field_name( 'link_vk' ); ?>" type="text" value="<?php echo esc_attr( $link_vk ); ?>">
+		</p>
+		
+		<?php 
+	}
+
+	/**
+	 * Сохранение настроек виджета. Здесь данные должны быть очищены и возвращены для сохранения их в базу данных.
+	 *
+	 * @see WP_Widget::update()
+	 *
+	 * @param array $new_instance новые настройки
+	 * @param array $old_instance предыдущие настройки
+	 *
+	 * @return array данные которые будут сохранены
+	 */
+	function update( $new_instance, $old_instance ) {
+		$instance = array();
+		$instance['title'] = ( ! empty( $new_instance['title'] ) ) ? strip_tags( $new_instance['title'] ) : '';
+		$instance['link_facebook'] = ( ! empty( $new_instance['link_facebook'] ) ) ? strip_tags( $new_instance['link_facebook'] ) : '';
+		$instance['link_twitter'] = ( ! empty( $new_instance['link_twitter'] ) ) ? strip_tags( $new_instance['link_twitter'] ) : '';
+		$instance['link_youtube'] = ( ! empty( $new_instance['link_youtube'] ) ) ? strip_tags( $new_instance['link_youtube'] ) : '';
+		$instance['link_instagram'] = ( ! empty( $new_instance['link_instagram'] ) ) ? strip_tags( $new_instance['link_instagram'] ) : '';
+		$instance['link_vk'] = ( ! empty( $new_instance['link_vk'] ) ) ? strip_tags( $new_instance['link_vk'] ) : '';
+
+		return $instance;
+	}
+
+	// скрипт виджета
+	function add_social_network_widget_scripts() {
+		// фильтр чтобы можно было отключить скрипты
+		if( ! apply_filters( 'show_my_widget_script', true, $this->id_base ) )
+			return;
+
+		$theme_url = get_stylesheet_directory_uri();
+
+		wp_enqueue_script('my_widget_script', $theme_url .'/my_widget_script.js' );
+	}
+
+	// стили виджета
+	function add_social_network_widget_style() {
+		// фильтр чтобы можно было отключить стили
+		if( ! apply_filters( 'show_my_widget_style', true, $this->id_base ) )
+			return;
+		?>
+		<style type="text/css">
+			.my_widget a{ display:inline; }
+		</style>
+		<?php
+	}
+
+} 
+// конец класса Social_Network_Widget
+
+// регистрация Social_Network_Widget в WordPress
+function register_social_network_widget() {
+	register_widget( 'Social_Network_Widget' );
+}
+add_action( 'widgets_init', 'register_social_network_widget' );
+
+
+
+
 
 //***ПОДКЛЮЧЕНИЕ СТИЛЕЙ И СКРИПТОВ
 function enqueue_universal_style() {
@@ -206,6 +365,25 @@ function enqueue_universal_style() {
   wp_enqueue_style( 'Roboto-Slab', 'https://fonts.googleapis.com/css2?family=Roboto+Slab:wght@700&display=swap');
 }
 add_action( 'wp_enqueue_scripts', 'enqueue_universal_style' );
+
+
+//***ИЗМЕНЕНИЯ РАЗМЕРОВ И НАСТРОЕК ЭЛЕМЕНТОВ В ОБЛАКЕ ТЕГОВ
+add_filter( 'widget_tag_cloud_args', 'edit_widget_tag_cloud_args');
+//d функции edit_widget_tag_cloud_args которая меняет аргументы тегов
+function edit_widget_tag_cloud_args($args){
+	//меняем ед. измеренения на px
+	$args['unit'] = 'px';
+	//меняем самый большой размер
+	$args['smallest'] = '14';
+	//меняем самый маленький размер
+	$args['largest'] = '14';
+	//меняем количество
+	$args['number'] = '11';
+	//сортируем по количетву записей
+	$args['orderby'] = 'count';
+  return $args;
+}
+
 
 ## отключаем создание миниатюр файлов для указанных размеров
 add_filter( 'intermediate_image_sizes', 'delete_intermediate_image_sizes' );
