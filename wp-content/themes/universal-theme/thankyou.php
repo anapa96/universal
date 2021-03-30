eb486f70b0061940efb81a19c430dd69bc7aa982
anapa96 <ana.p.a@yandex.ru>
2021-03-30 23:23:54 +0300

    Урок-18. Форма подписки

---
+++ wp-content/themes/universal-theme/thankyou.php
@@ -0,0 +1,13 @@
+<?php
+/*
+Template Name: Страница благодарности
+Template Post Type: page
+*/
+
+// … остальной код шаблона
+get_header();?>
+<div class="container">
+<?php the_content();?>
+</div>
+<!-- /.container -->
+<?php get_footer();
