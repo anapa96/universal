4a73408d7774a1d3f231f521b6590b2eaddbe508
anapa96 <ana.p.a@yandex.ru>
2021-04-05 21:21:31 +0300

    Урок 22. Дополнительные поля. Плагин ACF

--- wp-content/themes/universal-theme/contacts.php
+++ wp-content/themes/universal-theme/contacts.php
@@ -12,19 +12,38 @@ get_header();?>
     ?>
    <div class="contacts-wrapper">
     <div class="left">
-      <p class="page-text">Через форму обратной связи</p>
-     <form action="#" class="contacts-form" method="POST">
+      <p class=contacts-title">Через форму обратной связи</p>
+ <!--    <form action="#" class="contacts-form" method="POST">
         <input name="contact_name" type="text" placeholder="Ваше имя" class="input contacts-input">
         <input name="contact_email" type="email" placeholder="Ваш Email" class="input contacts-input">
         <textarea name="contact_comment" id="" placeholder="Ваш вопрос" class="textarea contacts-textarea"></textarea>
         <button type="submit" class="button more">Отправить  <svg width="20" height="20" fill="#fff" class="icon">
             <use xlink:href="<?php //echo get_template_directory_uri()?>/assets/images/sprite.svg#arrow"></use>
           </svg></button>
-      </form>
+      </form>-->
       <?php the_content(); ?>
     </div>
     <!-- /.left -->
     <div class="right">
+      <h2 class="contacts-title">Или по этим контактам</h2>
+      <?php
+        $email = get_post_meta(get_the_ID(), 'email', true);
+        if ($email){
+          echo '<a href="mailto:' .$email . '">' .$email . '</a> ' ;
+        }
+
+        $address = get_post_meta(get_the_ID(), 'address', true);
+        if($address){
+          echo '<address>' . $address . '</address>'  ;
+        }
+
+        $phone = get_post_meta(get_the_ID(), 'phone', true);
+        if($phone){
+          echo '<a href="tel:' .$phone . '">' .$phone . '</a> ' ;
+        }
+      
+      ?>
+    <!-- /.contacts-title -->
     </div>
     <!-- /.right -->
    </div>
